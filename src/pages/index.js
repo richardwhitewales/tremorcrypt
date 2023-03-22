83064e1062759af1010a66e348cc87f99996cd02
Richard White Wales <richardwhitewales@gmail.com>
2023-03-23 00:08:36 +0200

    update

--- src/pages/index.js
+++ src/pages/index.js
@@ -61,7 +61,7 @@ export default function HomePage() {
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
         <meta name="description" content={pageDesc} />
         <meta name="keywords" content="Harpy Crypto, Harpycrypto, investment company, crypto investments, Bitcoin investments, investment plans, attractive returns, selected plan, cryptocurrency, digital currency, crypto finance" />
-        <meta name="theme-color" content="#282A3B" />
+        <meta name="theme-color" content="#FFFFFF" />
         <link rel="icon" type="image/png" href="/favicon.png?v=1" />
         <meta name="author" content="Harpy Cryto" />
         <title>{pageName}</title>

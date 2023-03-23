945965afd6fc64f51992346994e423852fcb4019
Richard White Wales <richardwhitewales@gmail.com>
2023-03-24 00:16:35 +0200

    update

--- src/components/dashboard/quick.js
+++ src/components/dashboard/quick.js
@@ -1,8 +1,41 @@
 import styles from '@/components/dashboard/Dashboard.module.css'
-import toCurrency from '@/components/utils/toCurrency'
-import { Bitcoin, Ethereum, Litecoin, Xrp } from 'iconsax-react'
+import { useAuth } from '@/firebase/fire_auth_context';
+import { useState } from 'react';
+import { toast } from "react-toastify";
+import { db } from '@/firebase/fire_config';
+import Loader from '@/components/loader/loader';
+import { doc, updateDoc } from 'firebase/firestore';
 
 export default function DashboardQuick() {
+    const [loading, setLoading] = useState(false);
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+    const [phoneNumber, setPhoneNumber] = useState("");
+    const { authUser } = useAuth();
+
+    const onProfileUpdate = async event => {
+        event.preventDefault();
+        setLoading(true);
+
+        const docRef = doc(db, "users", authUser.email);
+        await updateDoc(docRef, {
+            "firstName": firstName,
+            "lastName": lastName,
+            "phoneNumber": phoneNumber,
+        }).then(() => {
+            toast.success("Profile Updated.");
+            setLoading(false);
+        }).catch((error) => {
+            if (error.code == "not-found") {
+                toast.error("User not found");
+                setLoading(false);
+            } else {
+                toast.error(`Something is wrong: ${error.message}`);
+                setLoading(false);
+            }
+        });
+    }
+
     return (
         <div className="container">
             <div className="row">
@@ -13,9 +46,21 @@ export default function DashboardQuick() {
                                 <h5>Quick payments</h5>
                                 <hr />
                             </div>
-                            <div className="col-12">
-                                <div className="">
-                                    
+                            <div className="col-12 text-center">
+                                <div className="m-2">
+                                    <p className="mb-5">
+                                        Quickly make deposit or withdrawal to or from your Harpy Crypto account with ease!
+                                        This is design to provide quick hand exprience.
+                                    </p>
+
+                                    <div className="d-flex justify-content-around mb-3">
+                                        <button type="button" className={`btn btn-lg btn-light bg-white ${styles.qiuck_btn}`}>
+                                            <span>Deposit</span>
+                                        </button>
+                                        <button type="button" className={`btn btn-lg btn_primary black ${styles.qiuck_btn}`}>
+                                            <span>Withdraw</span>
+                                        </button>
+                                    </div>
                                 </div>
                             </div>
                         </div>
@@ -26,12 +71,61 @@ export default function DashboardQuick() {
                     <div className={`my-3 p-2 ${styles.card}`}>
                         <div className="row">
                             <div className="col-12">
-                                <h5>Quick update</h5>
+                                <h5>Quick profile</h5>
                                 <hr />
                             </div>
+
                             <div className="col-12">
-                                <div className="">
-                                    
+                                <div className="m-2">
+                                    <form onSubmit={onProfileUpdate}>
+                                        <div className="row">
+                                            <div className="col-md-6">
+                                                <div className="form-floating mb-3">
+                                                    <input
+                                                        type="text"
+                                                        required
+                                                        className="form-control bg_black_15 text-white border-dark"
+                                                        id="firstName"
+                                                        placeholder="Jon"
+                                                        onChange={(event) => setFirstName(event.target.value)}
+                                                    />
+                                                    <label htmlFor="firstName">First Name</label>
+                                                </div>
+                                            </div>
+
+                                            <div className="col-md-6">
+                                                <div className="form-floating mb-3">
+                                                    <input
+                                                        type="text"
+                                                        required
+                                                        className="form-control bg_black_15 text-white border-dark"
+                                                        id="lastName"
+                                                        placeholder="Doe"
+                                                        onChange={(event) => setLastName(event.target.value)}
+                                                    />
+                                                    <label htmlFor="lastName">Last Name</label>
+                                                </div>
+                                            </div>
+                                        </div>
+
+                                        <div className="form-floating mb-3">
+                                            <input
+                                                type="text"
+                                                required
+                                                className="form-control bg_black_15 text-white border-dark"
+                                                id="phoneNumber"
+                                                placeholder="Jon"
+                                                onChange={(event) => setPhoneNumber(event.target.value)}
+                                            />
+                                            <label htmlFor="phoneNumber">Phone Number</label>
+                                        </div>
+
+                                        <div className="w-100 mt-4 d-flex justify-content-around">
+                                            <button type="submit" className="w-100 btn btn-lg btn-light bg-white">
+                                                {loading ? <Loader /> : "Save"}
+                                            </button>
+                                        </div>
+                                    </form>
                                 </div>
                             </div>
                         </div>

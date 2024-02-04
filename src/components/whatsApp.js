be0afdc33899ea3c81e465330bb40cd3db65d3db
Richard White Wales <richardwhitewales@gmail.com>
2024-02-04 17:14:51 +0200

    upd

--- src/components/whatsApp.js
+++ src/components/whatsApp.js
@@ -3,12 +3,22 @@ import Image from 'next/image'
 export default function WhatsApp() {
     return (
         <div className="position-fixed bottom-0 ms-2 mb-2">
-            <a href="https://wa.me/+2349157575590" target="_blank" rel="noreferrer">
+            <a href="https://wa.me/+2349157575590" className='me-2' target="_blank" rel="noreferrer">
                 <Image
                     src="/telegram.png"
                     alt="telegram"
                     width={50}
                     height={50}
+                    priority={true}
+                />
+            </a>
+
+            <a href="https://signal.me/#p/+447384270478" className='me-2' target="_blank" rel="noreferrer">
+                <Image
+                    src="/signal.png"
+                    alt="signal"
+                    width={80}
+                    height={80}
                     priority={true}
                 />
             </a>

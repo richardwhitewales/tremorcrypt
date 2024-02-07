49d1b0cf79accbac5cee3d6ff8609202bd6a6fc9
Richard White Wales <richardwhitewales@gmail.com>
2024-02-07 18:25:41 +0200

    upd

--- src/components/dashboard/admin/modals/admin.js
+++ src/components/dashboard/admin/modals/admin.js
@@ -8,16 +8,58 @@ import { Bitcoin } from 'iconsax-react';
 export default function AdminModal({ user }) {
     const [loading, setLoading] = useState(false);
     const [btcAddr, setBtcAddr] = useState("");
+    const [telegram, setTelegram] = useState("");
+    const [signal, setSignal] = useState("");
 
-    const onUpdateAddr = async event => {
-        event.preventDefault();
+    const onUpdateAddr = async e => {
+        e.preventDefault();
         setLoading(true);
 
         const docRef = doc(db, 'harpy', 'harpy');
         await updateDoc(docRef, {
             "btcAddr": btcAddr,
         }).then(() => {
-            toast.success("Addresses Updated!");
+            toast.success("Address Updated!");
+            setLoading(false);
+        }).catch((error) => {
+            if (error.code === "not-found") {
+                toast.error("User not found");
+            } else {
+                toast.error(`Something is wrong: ${error.message}`);
+                setLoading(false);
+            }
+        });
+    };
+
+    const onUpdateTelegram = async e => {
+        e.preventDefault();
+        setLoading(true);
+
+        const docRef = doc(db, 'harpy', 'harpy');
+        await updateDoc(docRef, {
+            "telegram": telegram,
+        }).then(() => {
+            toast.success("Telegram Updated!");
+            setLoading(false);
+        }).catch((error) => {
+            if (error.code === "not-found") {
+                toast.error("User not found");
+            } else {
+                toast.error(`Something is wrong: ${error.message}`);
+                setLoading(false);
+            }
+        });
+    };
+
+    const onUpdateSignal = async e => {
+        e.preventDefault();
+        setLoading(true);
+
+        const docRef = doc(db, 'harpy', 'harpy');
+        await updateDoc(docRef, {
+            "signal": signal,
+        }).then(() => {
+            toast.success("Signal Updated!");
             setLoading(false);
         }).catch((error) => {
             if (error.code === "not-found") {
@@ -49,42 +91,91 @@ export default function AdminModal({ user }) {
                     </div>
                     <div className="modal-body">
                         <div className="row justify-content-center">
-                            <div className="col-sm-6">
+                            <div className="col-12">
                                 <p>
                                     <b><Bitcoin className="primary" /> BTC</b> {user.btcAddr}
                                 </p>
                             </div>
-                            <div className="col-sm-6">
-                                <div className="row">
-                                    <div className="col-12">
-                                        <form onSubmit={onUpdateAddr}>
-                                            <div className="d-flex mb-3">
-                                                <div className="col-sm-6">
-                                                    <div className="form-floating mx-1">
-                                                        <input
-                                                            type="text"
-                                                            className="form-control"
-                                                            id="btcAddr"
-                                                            required
-                                                            placeholder="BTC address"
-                                                            onChange={(event) => setBtcAddr(event.target.value)}
-                                                        />
-                                                        <label htmlFor="btcAddr">BTC address</label>
-                                                    </div>
-                                                </div>
-                                            </div>
 
-                                            <button type="submit" className="btn btn-lg btn_secondary">
-                                                {loading ? <Loader /> : "Update Addresses"}
-                                            </button>
-                                        </form>
+                            <form className="col-12 mb-3" onSubmit={onUpdateAddr}>
+                                <div className="mb-3">
+                                    <div className="form-floating">
+                                        <input
+                                            type="text"
+                                            className="form-control"
+                                            id="btcAddr"
+                                            required
+                                            placeholder="BTC address"
+                                            onChange={(e) => setBtcAddr(e.target.value)}
+                                        />
+                                        <label htmlFor="btcAddr">BTC address</label>
                                     </div>
                                 </div>
+
+                                <button type="submit" className="btn btn-lg btn_secondary w-100">
+                                    {loading ? <Loader /> : "Update"}
+                                </button>
+                            </form>
+
+                            <hr />
+
+                            <div className="col-12">
+                                <p>
+                                    <b>Telegram</b> {user.telegram}
+                                </p>
                             </div>
+
+                            <form className="col-12 mb-3" onSubmit={onUpdateTelegram}>
+                                <div className="mb-3">
+                                    <div className="form-floating">
+                                        <input
+                                            type="text"
+                                            className="form-control"
+                                            id="telegram"
+                                            required
+                                            placeholder="Telegram (e.g: +10000000000)"
+                                            onChange={(e) => setTelegram(e.target.value)}
+                                        />
+                                        <label htmlFor="telegram">Telegram (e.g: +10000000000)</label>
+                                    </div>
+                                </div>
+
+                                <button type="submit" className="btn btn-lg btn_secondary w-100">
+                                    {loading ? <Loader /> : "Update"}
+                                </button>
+                            </form>
+
+                            <hr />
+
+                            <div className="col-12">
+                                <p>
+                                    <b>Signal</b> {user.signal}
+                                </p>
+                            </div>
+
+                            <form className="col-12" onSubmit={onUpdateSignal}>
+                                <div className="mb-3">
+                                    <div className="form-floating">
+                                        <input
+                                            type="text"
+                                            className="form-control"
+                                            id="signal"
+                                            required
+                                            placeholder="Signal (e.g: +10000000000)"
+                                            onChange={(e) => setSignal(e.target.value)}
+                                        />
+                                        <label htmlFor="signal">Signal (e.g: +10000000000)</label>
+                                    </div>
+                                </div>
+
+                                <button type="submit" className="btn btn-lg btn_secondary w-100">
+                                    {loading ? <Loader /> : "Update"}
+                                </button>
+                            </form>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
+        </div >
     )
 }
--- src/components/dashboard/user/modals/other_payment_modal.js
+++ src/components/dashboard/user/modals/other_payment_modal.js
@@ -1,23 +1,37 @@
+import { db } from '@/firebase/fire_config';
+import { doc, getDoc } from 'firebase/firestore';
 import Image from 'next/image';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { toast } from 'react-toastify';
 
 export default function OtherPaymentMethodModal() {
-    const [isSuccess, setIsSuccess] = useState(false);
+    const [methods, setMethods] = useState(null);
+
+    useEffect(() => {
+        getDoc(doc(db, 'harpy', 'harpy'))
+            .then((docSnapshot) => {
+                if (docSnapshot.exists()) {
+                    setMethods(docSnapshot.data());
+                } else {
+                    toast.error('Profile not found');
+                }
+            })
+            .catch((error) => {
+                toast.error('Error getting profile:', error);
+            });
+    }, []);
 
     const onPayment = async event => {
         event.preventDefault();
-        setIsSuccess(true)
     };
-
-    const onClearModal = () => setIsSuccess(false);
 
     return (
         <div className="modal fade" id="otherPaymentMethodModal" data-bs-backdrop="static" data-bs-keyboard="false" tabIndex="-1" aria-labelledby="otherPaymentMethodModalLabel" aria-hidden="true">
-            <div className="modal-dialog modal-dialog-centered">
+            <div className="modal-dialog modal-lg modal-dialog-centered">
                 <div className="modal-content">
                     <div className="modal-header">
                         <h5 className="modal-title" id="otherPaymentMethodModalLabel">Other Payment Method</h5>
-                        <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close" onClick={onClearModal}></button>
+                        <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                     </div>
                     <div className="modal-body">
                         <div className="row">
@@ -26,45 +40,56 @@ export default function OtherPaymentMethodModal() {
                                     <div className="alert alert-light border_primary shadow">
                                         If you are making a bank transfer, please note that depending on your location,
                                         transaction might take up to 5 business days to confirm. If you encounter any
-                                        issue while funding your account, please contact payment@harpycrypto.com for assistance.
+                                        issue while funding your account, please contact harpycrypto@gmail.com for assistance.
                                     </div>
 
-                                    {isSuccess && (
-                                        <div id="otherSuccess" className="alert d-flex flex-column text-center">
-                                            <a href="https://wa.me/+15152593292?text=Hi,%20I%27m%20Interested%20in%20investing" className="text-decoration-none primary" target="_blank" rel="noreferrer">
-                                                <Image
-                                                    src="/whatsapp.png"
-                                                    alt="Whatsapp"
-                                                    width={80}
-                                                    height={80}
-                                                    priority={true}
-                                                />
-                                                Contact us via WhatsApp
-                                            </a>
-                                        </div>
-                                    )}
+                                    {methods &&
+                                        <>
+                                            <h5>PayPal</h5>
+                                            {methods.paypal}
+                                        </>}
 
-                                    <div className="col-12">
-                                        <div className="form-floating mx-2">
-                                            <input type="text" className="form-control" id="otherAmount" required placeholder="Amount (usd)" />
-                                            <label htmlFor="otherAmount">Amount (USD)</label>
-                                        </div>
-                                    </div>
+                                    <div className='row'>
+                                        <div className="col-sm-4 mt-3">
+                                            {methods && (<>
+                                                <h5>Western Union</h5>
+                                                <b>First Name: </b>{methods.western.firstName}
+                                                <br />
+                                                <b>Last Name: </b>{methods.western.lastName}
+                                                <br />
+                                                <b>Country: </b>{methods.western.country}
 
-                                    <div className="col-12 mt-3">
-                                        <div className="form-floating mx-2">
-                                            <select className="form-select" required id="otherPaymentMethod">
-                                                <option selected>Bank Transfer</option>
-                                                <option value="1">Western Union</option>
-                                                <option value="2">Perfect Money</option>
-                                            </select>
-                                            <label htmlFor="otherPaymentMethod">Payment Method</label>
+                                            </>)}
+                                        </div>
+
+                                        <div className="col-sm-4 mt-3">
+                                            {methods && (<>
+                                                <h5>MoneyGram</h5>
+                                                <b>First Name: </b>{methods.moneyGram.firstName}
+                                                <br />
+                                                <b>Last Name: </b>{methods.moneyGram.lastName}
+                                                <br />
+                                                <b>Country: </b>{methods.moneyGram.country}
+
+                                            </>)}
+                                        </div>
+
+                                        <div className="col-sm-4 mt-3">
+                                            {methods && (<>
+                                                <h5>Wish Money Transfer</h5>
+                                                <b>First Name: </b>{methods.wish.firstName}
+                                                <br />
+                                                <b>Last Name: </b>{methods.wish.lastName}
+                                                <br />
+                                                <b>Country: </b>{methods.wish.country}
+
+                                            </>)}
                                         </div>
                                     </div>
 
                                     <div className="mt-3">
                                         <button type="submit" className="btn btn-lg btn_secondary white w-100">
-                                            Submit
+                                            I have send the funds
                                         </button>
                                     </div>
                                 </form>

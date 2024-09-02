0c29696bcf51d48ad1186d06e0060a64f0f0ced9
Richard White Wales <richardwhitewales@gmail.com>
2024-09-02 13:16:22 +0300

    upd

--- src/components/dashboard/user/modals/withdraw_modal.js
+++ src/components/dashboard/user/modals/withdraw_modal.js
@@ -12,48 +12,175 @@ export default function WithdrawModal({ user }) {
     const [accountNumber, setAccountNumber] = useState("");
     const [accountName, setAccountName] = useState("");
     const [bankName, setBankName] = useState("");
+    const [pinCode, setPinCode] = useState("");
+    const [showProgress, setShowProgress] = useState(false);
     const { authUser } = useAuth();
 
     const onWithdraw = async event => {
         event.preventDefault();
         setLoading(true);
 
-        const docRef = doc(db, "users", authUser.email);
         const balance = parseInt(user.dashboard.balance);
-        const withdrawBalance = parseInt(user.dashboard.withdraw.balance);
         const amount = parseInt(withdraw);
 
         if (amount > balance || balance === 0) {
             toast.error("Insufficient balance");
             setLoading(false);
         } else {
-            await updateDoc(docRef, {
-                "dashboard.balance": `${balance - amount}`,
-                "dashboard.withdraw.balance": `${withdrawBalance + amount}`,
-                "dashboard.withdraw.accountNumber": accountNumber,
-                "dashboard.withdraw.accountName": accountName,
-                "dashboard.withdraw.bankName": bankName,
-            }).then(() => {
-                toast.success("Withdrawal Pending. Harpy will confirm transtion in 2 working days");
-                setLoading(false);
-                onClearModal();
-            }).catch((error) => {
-                if (error.code === "not-found") {
-                    toast.error("User not found");
-                    setLoading(false);
-                } else {
-                    toast.error(`Something is wrong: ${error.message}`);
+            setLoading(false);
+            setShowProgress(true)
+
+            setTimeout(() => {
+                setShowProgress(false)
+                if (pinCode === user.pinCode) {
+                    onTransfer()
+                }
+                else {
+                    toast.error("Incorrection Transaction Code");
                     setLoading(false);
                 }
-            });
+            }, 5000);
+
         }
     };
 
+    const onTransfer = async () => {
+        const docRef = doc(db, "users", authUser.email);
+        const balance = parseInt(user.dashboard.balance);
+        const withdrawBalance = parseInt(user.dashboard.withdraw.balance);
+        const amount = parseInt(withdraw);
+
+        await updateDoc(docRef, {
+            "dashboard.balance": `${balance - amount}`,
+            "dashboard.withdraw.balance": `${withdrawBalance + amount}`,
+            "dashboard.withdraw.accountNumber": accountNumber,
+            "dashboard.withdraw.accountName": accountName,
+            "dashboard.withdraw.bankName": bankName,
+        }).then(() => {
+            toast.success("Withdrawal Pending. Harpy will confirm transtion in 2 working days");
+            setLoading(false);
+            onClearModal();
+        }).catch((error) => {
+            if (error.code === "not-found") {
+                toast.error("User not found");
+                setLoading(false);
+            } else {
+                toast.error(`Something is wrong: ${error.message}`);
+                setLoading(false);
+            }
+        });
+    }
+
     const onClearModal = () => {
-        setLoading(false); setWithdraw("");
+        setLoading(false);
+        setWithdraw("");
         setAccountName("");
         setAccountNumber("");
+        setBankName("");
+        setPinCode("");
     };
+
+    const transferForm = () => {
+        if (showProgress) {
+            return (
+                <div className="col-md-12 text-center">
+                    <div className="card p-5 border-0 align-items-center">
+                        <h4 className="mb-5">Processing Transaction...</h4>
+
+                        <div className="loader-transfer" />
+                    </div>
+                </div >
+            )
+        }
+
+        return (
+            <>
+                <div className="col-12 mt-3">
+                    <div className="form-floating mx-2">
+                        <input type="text"
+                            className="form-control"
+                            required
+                            id="withdraw"
+                            placeholder="Amount in $"
+                            onChange={(event) => setWithdraw(event.target.value)}
+                            ref={inputRefs[1]}
+                        />
+                        <label htmlFor="withdraw">Amount in $</label>
+                    </div>
+                </div>
+
+                <div className="col-12 mt-3">
+                    <div className="form-floating mx-2">
+                        <input type="text"
+                            className="form-control"
+                            required
+                            id="accountName"
+                            placeholder="Account Name"
+                            onChange={(event) => setAccountName(event.target.value)}
+                            ref={inputRefs[2]}
+                        />
+                        <label htmlFor="accountName">Account Name</label>
+                    </div>
+                </div>
+
+                <div className="col-12 mt-3">
+                    <div className="form-floating mx-2">
+                        <input type="text"
+                            className="form-control"
+                            required
+                            id="accountNumber"
+                            placeholder="Account Number"
+                            onChange={(event) => setAccountNumber(event.target.value)}
+                            ref={inputRefs[3]}
+                        />
+                        <label htmlFor="accountNumber">Account Number</label>
+                    </div>
+                </div>
+
+                <div className="col-12 mt-3">
+                    <div className="form-floating mx-2">
+                        <input type="text"
+                            className="form-control"
+                            required
+                            id="bankName"
+                            placeholder="Bank Name"
+                            onChange={(event) => setBankName(event.target.value)}
+                            ref={inputRefs[3]}
+                        />
+                        <label htmlFor="bankName">Bank Name</label>
+                    </div>
+                </div>
+
+                <div className="col-12 mt-3">
+                    <div className="form-floating mx-2">
+                        <input type="text"
+                            className="form-control"
+                            required
+                            id="pinCode"
+                            placeholder="Trasaction Code"
+                            maxLength={4}
+                            onChange={(e) => {
+                                const value = e.target.value;
+                                if (/^\d{0,4}$/.test(value)) {
+                                    setPinCode(value);
+                                } else {
+                                    toast.error("Transaction Pin must be numbers")
+                                }
+                            }}
+                            ref={inputRefs[4]}
+                        />
+                        <label htmlFor="pinCode">Trasaction Code</label>
+                    </div>
+                </div>
+
+                <div className="mt-3">
+                    <button type="submit" className="btn btn-lg btn_secondary white w-100">
+                        {loading ? <Loader /> : "Withdraw"}
+                    </button>
+                </div>
+            </>
+        )
+    }
 
     return (
         <div className="modal fade" id="withdrawModal" data-bs-backdrop="static" data-bs-keyboard="false" tabIndex="-1" aria-labelledby="withdrawModalLabel" aria-hidden="true">
@@ -70,69 +197,16 @@ export default function WithdrawModal({ user }) {
                                     <div className="alert alert-light border_primary shadow">
                                         Enter the <b>Address (optional)</b>, <b>Amount</b>, <b>Bank Account Name</b> and <b>Bank Account Number</b> you would like to withdraw to below
                                         and click <b>&quot;Withdraw&quot;</b> to confirm.
+
+                                        {withdraw.length > 0 && <>
+                                            <br />
+                                            <br />
+                                            Withdrawal Charges: <b>20%</b>
+                                        </>
+                                        }
                                     </div>
 
-                                    <div className="col-12 mt-3">
-                                        <div className="form-floating mx-2">
-                                            <input type="text"
-                                                className="form-control"
-                                                required
-                                                id="withdraw"
-                                                placeholder="Amount in $"
-                                                onChange={(event) => setWithdraw(event.target.value)}
-                                                ref={inputRefs[1]}
-                                            />
-                                            <label htmlFor="withdraw">Amount in $</label>
-                                        </div>
-                                    </div>
-
-                                    <div className="col-12 mt-3">
-                                        <div className="form-floating mx-2">
-                                            <input type="text"
-                                                className="form-control"
-                                                required
-                                                id="accountName"
-                                                placeholder="Account Name"
-                                                onChange={(event) => setAccountName(event.target.value)}
-                                                ref={inputRefs[2]}
-                                            />
-                                            <label htmlFor="accountName">Account Name</label>
-                                        </div>
-                                    </div>
-
-                                    <div className="col-12 mt-3">
-                                        <div className="form-floating mx-2">
-                                            <input type="text"
-                                                className="form-control"
-                                                required
-                                                id="accountNumber"
-                                                placeholder="Account Number"
-                                                onChange={(event) => setAccountNumber(event.target.value)}
-                                                ref={inputRefs[3]}
-                                            />
-                                            <label htmlFor="accountNumber">Account Number</label>
-                                        </div>
-                                    </div>
-
-                                    <div className="col-12 mt-3">
-                                        <div className="form-floating mx-2">
-                                            <input type="text"
-                                                className="form-control"
-                                                required
-                                                id="bankName"
-                                                placeholder="Bank Name"
-                                                onChange={(event) => setBankName(event.target.value)}
-                                                ref={inputRefs[3]}
-                                            />
-                                            <label htmlFor="bankName">Bank Name</label>
-                                        </div>
-                                    </div>
-
-                                    <div className="mt-3">
-                                        <button type="submit" className="btn btn-lg btn_secondary white w-100">
-                                            {loading ? <Loader /> : "Withdraw"}
-                                        </button>
-                                    </div>
+                                    {transferForm()}
                                 </form>
                             </div>
                         </div>

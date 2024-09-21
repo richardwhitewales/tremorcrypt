bab3b283d3fba768eddaf6ad16677d4b82cf03df
Richard White Wales <richardwhitewales@gmail.com>
2024-09-21 19:01:40 +0300

    upd

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,7 +4,21 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <script src="//code.jivosite.com/widget/jbKLUMnwHD" async />
+      <script
+          type="text/javascript"
+          dangerouslySetInnerHTML={{
+            __html: `
+                var _smartsupp = _smartsupp || {};
+                _smartsupp.key = 'f5acb7fcd05f8c3af730bb9f2888a41ad1dec0d6';
+                window.smartsupp||(function(d) {
+                  var s,c,o=smartsupp=function(){ o._.push(arguments)};o._=[];
+                  s=d.getElementsByTagName('script')[0];c=d.createElement('script');
+                  c.type='text/javascript';c.charset='utf-8';c.async=true;
+                  c.src='https://www.smartsuppchat.com/loader.js?';s.parentNode.insertBefore(c,s);
+                })(document);
+              `
+          }}
+        />
       </Head>
       <body>
         <Main />
